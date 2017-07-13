ad9517a779d33896f24402eca22bb4726c072f12
Andrei <andreicrimson@gmail.com>
2017-07-13 13:37:11 -0700

    removed home page duplicate

--- app.js
+++ app.js
@@ -9,18 +9,6 @@ app.set('port', process.env.PORT || 8080);
 //Connect all the routes to the application
 app.use('/', routes);
 
-//Initial page
-app.get('/', (request, response) => {
-    response.send('Hello World');
-});
-
-//Endpoint for retrieving a 'note'
-app.get('/note', (request, response) => {
-    response.json({
-        notes: "This is a random note"
-    });
-});
-
 http.createServer(app).listen(app.get('port'), () => {
     console.log('Express server listening on port ' + app.get('port'));
 });
